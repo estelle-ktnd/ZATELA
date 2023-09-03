0b7aa17fb2769228cfad9481276f9f993d43b87e
Red6Intox <65827011+estelle-ktnd@users.noreply.github.com>
2023-09-03 05:03:11 +0200

    Add files via upload

---
+++ global.js
@@ -0,0 +1,116 @@
+/* Menu mobile */
+
+function menuMobile() {
+  const btn = document.querySelector('.burger');
+  const header = document.querySelector('.header');
+  const links = document.querySelectorAll('.navbar a');
+
+  btn.addEventListener('click', () => {
+    header.classList.toggle('show-nav');
+  });
+
+  links.forEach(link => {
+    link.addEventListener('click', () => {
+      header.classList.remove('show-nav');
+    });
+  });
+}
+
+menuMobile();
+
+/* Porfolio */
+
+function tabsFilters() {
+  const tabs = document.querySelectorAll('.location-filters a');
+  const projets = document.querySelectorAll('.location .card');
+
+  const resetActiveLinks = () => {
+    tabs.forEach(elem => {
+      elem.classList.remove('active');
+    })
+  }
+
+  const showProjets = (elem) => {
+    console.log(elem);
+    projets.forEach(projet => {
+
+      let filter = projet.getAttribute('data-category');
+
+      if (elem === 'all') {
+        projet.parentNode.classList.remove('hide');
+        return
+      }
+
+      if (filter !== elem) {
+        projet.parentNode.classList.add('hide');
+      } else {
+        projet.parentNode.classList.remove('hide');
+      }
+
+      filter !== elem ? projet.parentNode.classList.add('hide') : projet.parentNode.classList.remove('hide');
+
+    });
+  }
+
+  tabs.forEach(elem => {
+    elem.addEventListener('click', (event) => {
+      event.preventDefault();
+      let filter = elem.getAttribute('data-filter');
+      showProjets(filter)
+      resetActiveLinks();
+      elem.classList.add('active');
+    });
+  })
+}
+
+tabsFilters()
+
+function showProjectDetails() {
+  const links = document.querySelectorAll('.card__link');
+  const modals = document.querySelectorAll('.modal');
+  const btns = document.querySelectorAll('.modal__close');
+
+  const hideModals = () => {
+    modals.forEach(modal => {
+      modal.classList.remove('show');
+    });
+  }
+
+  links.forEach(elem => {
+    elem.addEventListener('click', (event) => {
+      event.preventDefault();
+      document.querySelector(`[id=${elem.dataset.id}]`).classList.add('show');
+    });
+  });
+
+  btns.forEach(btn => {
+    btn.addEventListener('click', (event) => {
+      hideModals();
+    });
+  });
+
+}
+
+showProjectDetails();
+
+
+//SlideShow
+let slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}    
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
+  setTimeout(showSlides, 3000); // Change image every 3 seconds
+}
